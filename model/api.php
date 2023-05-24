89a56cd903f671fcdc37c0c06898803668bc1142
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-05-24 08:36:59 -0500

    feat: :sparkles: json-server

--- model/api.php
+++ model/api.php
@@ -1,10 +1,11 @@
 <?php
-
+    header("Content-Type: application/json");
     function autoload($class){
         require 'entidad/'.$class.'.php';
     }
     spl_autoload_register('autoload');
 
 
-    echo info::getinfo();
+    $res = new user("nicolas", "321M");
+    echo $res->getUser();
 ?>
--- model/entidad/user.php
+++ model/entidad/user.php
@@ -1,7 +1,18 @@
 <?php
     class user{
-        static public function getUser():string{
-            return "User";
+        static $config = array(
+            "http"=>["header"=>"Content-Type: application/json"]
+        );
+        public $usuario;
+        protected $contrasena;
+        public function __construct($usuario, $contrasena){
+            $this->usuario = $usuario;
+            $this->contrasena = $contrasena;
+        }
+        public function getUser(){
+            self::$config["http"]["method"] = "GET";
+            $res = file_get_contents("http://localhost:4000/users?nombre=".$this->usuario."&pw=".$this->contrasena, false, stream_context_create(self::$config));
+            return $res;
         }
     }
 ?>
